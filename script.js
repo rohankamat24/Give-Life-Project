2f026edd71810c29ccf3a227c57b749c37ad7283
rohankamat24 <ROHANKAMAT87000@GMAIL.COM>
2025-03-24 22:37:06 +0530

    Front-End Code for Give Life Website.

---
+++ script.js
@@ -0,0 +1,10 @@
+function contactUs() {
+    alert("Feel free to reach out to us via email at er.vanshnagpal@gmail.com!");
+}
+
+function checkAvailability() {
+    alert("Redirecting to organ banks and availability.");
+}
+function locateHospitals() {
+    alert("Finding transplant centers for you.");
+}
